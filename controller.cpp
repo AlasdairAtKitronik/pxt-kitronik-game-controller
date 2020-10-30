cdb7a61d56366b32b77d2c9faad4c45df577b218
AlasdairAtKitronik <alasdair.trench@kitronik.co.uk>
2020-10-30 15:10:06 +0000

    Changed button init method & upversion 0.0.5

--- controller.cpp
+++ controller.cpp
@@ -16,14 +16,22 @@ namespace Kitronik_Game_Controller {
 
         #if MICROBIT_CODAL
             //This function sets all the buttons on the :GAME Controller to actually appear as buttons on the micro:bit
-            #define ALLOC_PIN_BUTTON(id) new Button(getPin(id)->name, id, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW);
+            /*#define ALLOC_PIN_BUTTON(id) new Button(getPin(id)->name, id, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW);
                 ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P8)
                 ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
                 ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
                 ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
                 ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
                 ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
-            #undef ALLOC_PIN_BUTTON
+            #undef ALLOC_PIN_BUTTON*/
+            Button          buttonP8;
+            //Button          buttonP12;
+            //Button          buttonP13;
+            //Button          buttonP14;
+            //Button          buttonP15;
+            //Button          buttonP16;
+
+            buttonP8(io.P8, DEVICE_ID_IO_P8, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW);
         #else
             //This function sets all the buttons on the :GAME Controller to actually appear as buttons on the micro:bit
             #define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
